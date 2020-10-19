7df8363fadd76fc7d7a0b7ebe1a67878c1e2dc26
welbertSilva <welbert.silva@gmail.com>
2020-10-19 12:16:26 -0300

    Create firts migration appointments backend projetc

---
+++ backend/src/database/migrations/1603120407172-CreateAppointments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export default class CreateAppointments1603120407172 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,13 @@
 import express, { json } from  'express';
 import routes from './routes'
+
 import './database';
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333,() =>{
     console.log('Server nodeJS UP on port 3333');
-})
+});
