6175ccda0c211d55182c082ef665eb3433587522
welbertSilva <welbert.silva@gmail.com>
2020-11-05 20:47:16 -0300

    Create migration AddAvatarFieldToUsers.

---
+++ backend/src/database/migrations/1604619928168-AddAvatarFieldToUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddAvatarFieldToUsers1604619928168 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
